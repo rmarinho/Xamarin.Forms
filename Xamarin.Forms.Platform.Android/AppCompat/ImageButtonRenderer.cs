e022911acbcb0fd56596d4d2950740e027d0d934
ShaneN <shane94@hotmail.com>
2019-09-06 17:16:10 -0600

    - adding animation handler for android

--- Xamarin.Forms.Platform.Android/AppCompat/ImageButtonRenderer.cs
+++ Xamarin.Forms.Platform.Android/AppCompat/ImageButtonRenderer.cs
@@ -33,8 +33,7 @@ namespace Xamarin.Forms.Platform.Android
 		VisualElementRenderer _visualElementRenderer;
 		BorderBackgroundManager _backgroundTracker;
 		IPlatformElementConfiguration<PlatformConfiguration.Android, ImageButton> _platformElementConfiguration;
-		ImageButton _imageButton;
-		IFormsAnimationDrawable _formsAnimationDrawable;
+		ImageButton _imageButton;		
 
 		public event EventHandler<VisualElementChangedEventArgs> ElementChanged;
 		public event EventHandler<PropertyChangedEventArgs> ElementPropertyChanged;
@@ -330,14 +329,6 @@ namespace Xamarin.Forms.Platform.Android
 
 		void IImageRendererController.SetFormsAnimationDrawable(IFormsAnimationDrawable value)
 		{
-			if (_formsAnimationDrawable != null)
-				_formsAnimationDrawable.AnimationStopped -= OnAnimationStopped;
-
-			_formsAnimationDrawable = value;
-			if (_formsAnimationDrawable != null)
-				_formsAnimationDrawable.AnimationStopped += OnAnimationStopped;
 		}
-		void OnAnimationStopped(object sender, FormsAnimationDrawableStateEventArgs e) =>
-			ImageElementManager.OnAnimationStopped(Element, e);
 	}
 }
